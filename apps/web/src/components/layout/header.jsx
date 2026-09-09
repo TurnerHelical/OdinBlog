9d498fa6ae3921aa0d78761bf4046e37dd7141bd
unknown <hunterde91@gmail.com>
2026-09-09 16:10:07 -0400

    changed what logout button in header does

--- apps/web/src/components/layout/header.jsx
+++ apps/web/src/components/layout/header.jsx
@@ -1,30 +1,86 @@
-import {Link, useRouteLoaderData} from 'react-router';
-import '../../styles/header.css'
+import {
+    Link,
+    useRouteLoaderData,
+    useRevalidator,
+    useNavigate
+} from 'react-router';
+
+import { api, setAccessToken } from '../../helpers/apiHelper';
+import '../../styles/header.css';
 
 const Header = () => {
-    const rootData = useRouteLoaderData('root');    
+    const rootData = useRouteLoaderData('root');
+    const revalidator = useRevalidator();
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+        try {
+            await api({
+                url: '/auth/logout',
+                options: {
+                    method: 'POST',
+                },
+            });
+
+            setAccessToken(null);
+
+            navigate('/auth', { replace: true });
+            revalidator.revalidate();
+
+        } catch (error) {
+            console.error('Logout failed:', error);
+        }
+    }
+
     return (
         <>
-        <div id='headerContainer'>
-            <h1><Link to='/'>Odin Blog</Link></h1>
-            <div>
-                {rootData.user?.user 
-                ? (<div id='userHeader'>
-                    <Link to='/dash' id='headerDash' >{rootData.user.user.displayname}</Link>
-                    <Link to='/auth' id='headerAuth' >Logout</Link>
-                    </div>
-                )
-                : (<Link to='/auth'>Login / Signup</Link>)}
-            </div>
-        </div> 
-        <nav id='mainNav'>
-                <Link to='/'>Home</Link>
-                {rootData.user?.user && (<Link to='/dash'>Dashboard</Link>)}
-                {rootData.user?.user?.isAdmin && (<Link to='/adminPanel'>Admin Panel</Link>)}
+            <div id='headerContainer'>
+                <h1>
+                    <Link to='/'>Odin Blog</Link>
+                </h1>
 
+                <div>
+                    {rootData.user?.user
+                        ? (
+                            <div id='userHeader'>
+                                <Link
+                                    to='/dash'
+                                    id='headerDash'
+                                >
+                                    {rootData.user.user.displayname}
+                                </Link>
+
+                                <button
+                                    type='button'
+                                    id='headerAuth'
+                                    onClick={handleLogout}
+                                >
+                                    Logout
+                                </button>
+                            </div>
+                        )
+                        : (
+                            <Link to='/auth'>
+                                Login / Signup
+                            </Link>
+                        )
+                    }
+                </div>
+            </div>
+
+            <nav id='mainNav'>
+                <Link to='/'>Home</Link>
+
+                {rootData.user?.user && (
+                    <Link to='/dash'>Dashboard</Link>
+                )}
+
+                {rootData.user?.user?.isAdmin && (
+                    <Link to='/adminPanel'>Admin Panel</Link>
+                )}
             </nav>
         </>
-    )
-}
+    );
+};
 
 export default Header;
